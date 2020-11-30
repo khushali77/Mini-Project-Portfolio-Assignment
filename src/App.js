641a3a80a1bb8b5defa969d041532be3fcbd2b68
khushali77 <201801264@daiict.ac.in>
2020-11-30 22:46:55 +0530

    Added:Profile section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import "./css/App.css";
 import React from "react";
 import Header from "./components/header";
 import { BrowserRouter,Route } from "react-router-dom";
-import {Home,About,Contact} from "./Home";
+import {Home,About,Contact} from "./Pages";
+import Profile from "./components/profile";
 
 function App() {
   return (
+      <>
       <BrowserRouter>
         <Header/>
+        <Route exact path='/' component = {Home}/>
         <Route exact path='/Home' component = {Home}/>
         <Route exact path='/About' component = {About}/>
         <Route exact path='/Contact' component = {Contact}/>
       </BrowserRouter>
+      </>
   );
 }
 
---
+++ src/portfolioData.js
@@ -0,0 +1,8 @@
+export const Data = {
+    img:"https://instagram.fudr1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/127076523_670459453862237_8399630578754083759_n.jpg?_nc_ht=instagram.fudr1-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=01VvCniMpOsAX9ZZIxx&tp=1&oh=3ca64c65a2dee26de6e2f6958ebeff97&oe=5FF01977",
+    alt:"Profile Photo",
+    name:"Khushali Shah",
+    college:"DAIICT'22 | GLS'18",
+    desc:"Looming Summer Intern at Goldman Sachs'21"
+}
+
